b4f5a1ecf50c31544eda30d8c485a9349f8efa16
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-02-24 16:04:38 -0800

    VMware landing page
    Footnotes

--- source/javascripts/vagrantup.js
+++ source/javascripts/vagrantup.js
@@ -1,15 +1,28 @@
 // add dropshadow to nav on scroll
 $(document).ready(function(){
-  $(document).scroll(function() {
-    var top = $(document).scrollTop();
-    if (top > 0) $('nav').addClass("drop-shadow");
-    if (top === 0) $('nav').removeClass("drop-shadow");
-  });
+    $(document).scroll(function() {
+        var top = $(document).scrollTop();
+        if (top > 0) $('nav').addClass("drop-shadow");
+        if (top === 0) $('nav').removeClass("drop-shadow");
+    });
 });
 
 // open/close documentation side nav on small screens
 $(document).ready(function(){
-  $(".toggle").click(function() {
-    $(".sidebar-nav ul").slideToggle('slow');
-  });
+    $(".toggle").click(function() {
+        $(".sidebar-nav ul").slideToggle('slow');
+    });
+});
+
+// Redirect to the proper checkout screen for quantity
+$(document).ready(function() {
+    $("#buy-fusion").click(function() {
+        var seats = parseInt($("#seats").val(), 10);
+        if (isNaN(seats)) {
+            alert("The number of seats you want to purchase must be a number.");
+            return;
+        }
+
+        window.location = "http://shopify.hashicorp.com/cart/279661674:" + seats;
+    });
 });
